7259f66bfeb0d747f40e3924ba56183fc193ad05
shellking <donaldkant2000@gmail.com>
2021-03-25 11:28:05 +0100

    Last update and bendings

--- resources/views/parc/policiesView.blade.php
+++ resources/views/parc/policiesView.blade.php
@@ -24,16 +24,16 @@
             utiliser la voiture. Le total du montant de ses locations est alors mis à jour. Le client CaRRentAL paie ses dus de façon physique au moment de la récupération de la
             voiture dans les parcs CaRRentAL. Une fois les paiements réglés le client peut maintenant récupérer la voiture qu'il a louée.
             Le service clientèle de CaRRentAL est consititué de charmantes personnes gentilles, très accueillantes, et très à l'écoute des clients afin de les satisfaire et les aider pour tous leurs eventuels soucis.
-            Une fois la durée d'utilisation de la voiture terminée, le client devra se connecter sur la plateforme et rendre la voiture là-bas avant de la ramener dans le parc CaRRentAL.
+            Une fois la durée d'utilisation de la voiture terminée, le client devra se connecter sur la plateforme et rendre la voiture là-bas puis la ramener dans le parc CaRRentAL.
             <br>
             <h4 style="font-weight: bold; text-align: center; margin-bottom: 14px;">Politique de prêt de voitures</h4>
             <br>
             Comme dit plus haut, la plateforme CaRRentAL prend aussi en compte des prêts de voitures. Cela dit, une fois que vous avez un compte CaRRentAL, vous pouvez emprunter des voitures sur la plateforme. 
-            Une voiture prêtée à un client sur CaRRentAL doit être utilisée pendant 24h au maximum, durée au bout de laquelle la voiture devra être ramenée par le client dans le parc CaRRentAL. 
+            Une voiture prêtée à un client sur CaRRentAL doit être utilisée pendant deux jours (48h) au maximum, durée au bout de laquelle la voiture devra être ramenée par le client dans le parc CaRRentAL. 
             Pour pouvoir utiliser utiliser une voiture sur une plus longue durée il faut payer et la louer.
             Un client CaRRentAL ayant emprunté une voiture et qui ne l'a pas rendue au bout du temps convenu se verra dans un premier temps appelé par le service clientèle de la plateforme auquel il devra s'expliquer et fournir les détails de 
-            cette dérogation à nos règles. En cas de rétention de voiture par un client CaRRentAL pendant une longue durée, une poursuite judiciaire est engagée contre le client ayant emprunté la voiture.
-            Si aucune situation innattendue ne survient, alors une fois la durée d'utilisation de la voiture terminée, le client devra se connecter sur la plateforme et rendre la voiture là-bas avant de la ramener dans le parc CaRRentAL.
+            cette dérogation à nos règles. En cas de rétention de voiture par un client CaRRentAL pendant une longue durée, une poursuite judiciaire est engagée contre le client en question.
+            Si aucune situation innattendue ne survient, alors une fois la durée d'utilisation de la voiture terminée, le client devra se connecter sur la plateforme et rendre la voiture là-bas puis la ramener dans le parc CaRRentAL.
             <br>
             <h4 style="font-weight: bold; text-align: center; margin-bottom: 14px">Confidentialités CaRRentAL</h4>
             <br>
